59a902eab80f325895a3cefd69c4c796e2d8952f
Антон Красноперов <an2niy85@mail.ru>
2021-11-19 20:42:25 +0400

    Добавьте файлы проекта.

---
+++ SecretMessage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "123456";
+            string userInput;
+
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.WriteLine("Введите пароль:");
+                userInput = Console.ReadLine();
+
+                if (password == userInput)
+                {
+                    Console.WriteLine("Секретное сообщение");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("У вас осталось " + (tryCount - i) + " попыток.");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
